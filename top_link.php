b8e5fe5a3097d8f7420bd034c7a55f721adb2b1e
jiangshan79 <317726681@qq.com>
2017-06-07 15:46:17 +0800

    原版本

---
+++ top_link.php
@@ -0,0 +1,16 @@
+<!--
+<div style="background-color: #fff;">
+<table cellpadding="0" cellspacing="0" align="center" width="100%" >
+<tr>
+<td>
+<div style="font-family: 微软雅黑;font-size: 20px;color: #000;padding-bottom: 10px;padding-top: 10px;margin-left: 10px;">
+<?php echo $admin_rs['company']?>&nbsp;&nbsp;<span style="font-weight: normal;color: #ccc;font-size: 16px;">管理系统</span>
+</div>
+</td>
+<td class="out_link" width="100px" align="center">
+<a href="index.php">安全退出</a>
+</td>
+</tr>
+</table>
+</div>
+-->
